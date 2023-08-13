aacec9ba334706e467ef1ec26a6fda1a2d3296ea
wachaon <wachaon@gmail.com>
2023-08-14 00:21:33 +0900

    Update 0.2.0

--- usage.mjs
+++ usage.mjs
@@ -1,7 +1,7 @@
 // built-ins
 import { readFileSync } from 'filesystem'
 import { resolve } from 'pathname'
-import tester from 'markdownCodeTester'
+import tester from 'markdown-code-tester'
 
 const md = readFileSync(resolve(process.cwd(), 'README.md'), 'UTF-8')
 
